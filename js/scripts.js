2c7914958ec092f9ff683333f5e5abb5052d972d
Kaveh Saleminejad and Patricia Filipovic <kaveh@>
2018-10-18 15:29:41 -0700

    all done

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,27 @@
-// $(document).ready(function() {
-  (function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    console.log("testing");
-
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    console.log(forms);
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      console.log(form);
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})();
-// });
+$(document).ready(function() {
+  $("#blanks form").submit(function() {
+    event.preventDefault();
+    var name = $("input#name").val();
+    var gender = $("#gender").val();
+    var interest = $("#interest").val();
+    if (name) {
+      $(".name").text(name);
+      $(".story").show();
+      if (gender==0 && interest==0) {
+        $(".niki").show();
+      } else if (gender==0 && interest==1){
+        $(".charlize").show();
+      } else if (gender==0 && interest==2){
+        $(".maria").show();
+      } else if (gender==1 && interest==0){
+        $(".adam").show();
+      } else if (gender==1 && interest==1){
+        $(".chris").show();
+      } else if (gender==1 && interest==2){
+        $(".lebron").show();
+      }
+    } else {
+      alert("Please enter a name")
+    }
+  });
+});
